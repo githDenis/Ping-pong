082682f49a75eacef3cd7be282d64e0201d0b5b2
Котов Денис <kotovdd2008@gmail.com>
2023-07-11 17:00:09 +0300

    Improved the code

--- game.py
+++ game.py
@@ -8,20 +8,22 @@ BLACK = (0, 0, 0)
 RECT_SIZE = (80, 13)
 RADIUS = 10
 SPEED_RECT = 25
-SPEED_X_BALL = 4
-SPEED_Y_BALL = 3
-FPS = 60
+FPS = 90
+
+speed_x_ball = 3
+speed_y_ball = 2
 
 rect_pos = [WIN_SIZE[0] / 2 - RECT_SIZE[0] / 2, WIN_SIZE[1] - RECT_SIZE[1] * 2]
 ball_pos = [WIN_SIZE[0] / 2 + RADIUS / 2, WIN_SIZE[1] / 2 - RADIUS * 2]
 
-def run_game():
-    gameOver = False
-    
-    pygame.init()
-    speed_x_ball = SPEED_X_BALL
-    speed_y_ball = SPEED_Y_BALL
 
+def run_game():
+    global speed_x_ball
+    global speed_y_ball
+
+    gameOver = False
+
+    pygame.init()
     screen = pygame.display.set_mode(WIN_SIZE)
     clock = pygame.time.Clock()
     pygame.display.set_caption(WIN_TITLE)
@@ -40,15 +42,15 @@ def run_game():
                 rect_pos[0] += SPEED_RECT
 
         if ball_pos[0] + RADIUS > WIN_SIZE[0]:
-            speed_x_ball = -SPEED_X_BALL
+            speed_x_ball *= -1
         elif ball_pos[0] - RADIUS < 0:
-            speed_x_ball = SPEED_X_BALL
+            speed_x_ball *= -1
 
         if ball_pos[1] + RADIUS > WIN_SIZE[1]:
             print("Game over")
             gameOver = True
         elif ball_pos[1] - RADIUS < 0:
-            speed_y_ball = SPEED_Y_BALL
+            speed_y_ball *= -1
 
         if rect_pos[0] < 0:
             rect_pos[0] = 0
@@ -59,7 +61,7 @@ def run_game():
             ball_pos[0] + RADIUS >= rect_pos[0]
             and ball_pos[0] + RADIUS <= rect_pos[0] + 80
         ):
-            speed_y_ball = -SPEED_Y_BALL
+            speed_y_ball *= -1
 
         screen.fill(BLACK)
 
